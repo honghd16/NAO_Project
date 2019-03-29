1db680b6a1c3145d7377590856327fa96f4778a3
Ziniu Wan <wanraxxer@hotmail.com>
2019-03-29 13:38:46 +0800

    rm all .pyc files; add gitignore

--- test/move_and_talk.py
+++
@@ -1,9 +0,0 @@
-from naoqi import ALProxy
-import time 
-motion = ALProxy("ALMotion", "192.168.114.78", 9559)
-tts    = ALProxy("ALTextToSpeech", "192.168.114.78", 9559)
-motion.moveInit()
-motion.post.moveTo(0.2, 0, 0)
-time.sleep(4)
-motion.moveInit()
-#tts.say("I'm walking")
--- test/tts.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-  
-import qi
-
-session = qi.Session()
-session.connect("tcp://192.168.114.78:9559")
-tts = session.service("ALTextToSpeech")
-tts.setLanguage("Chinese")
-tts.say("你好")
-
